5ab1a1ebb01e7437f54b62edefd3fb76a9842f4c
yuchengtian <92207409+yuchengtian@users.noreply.github.com>
2021-10-10 14:39:31 +0800

    perf: 添加prettier配置

---
+++ lib/SchemaForm.tsx
@@ -0,0 +1,10 @@
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'SchemaForm',
+  setup(props, { slots, emit, attrs }) {
+    return () => {
+      ;<div>this is a form</div>
+    }
+  },
+})
