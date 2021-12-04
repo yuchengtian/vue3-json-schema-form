e0ddef927d9cfa026088350ce997e78cbb6159e9
wx_dengyiquan <1059914928@qq.com>
2021-12-04 13:37:46 +0800

    feat: 添加编辑器样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { defineComponent, ref, Ref } from 'vue'
 import MonacoEditor from './components/MonacoEditor'
+import { createUseStyles } from 'vue-jss'
 
 function toJson(data: any) {
   return JSON.stringify(data, null, 2)
@@ -8,6 +9,13 @@ function toJson(data: any) {
 const schema = {
   type: 'string',
 }
+
+const useStyles = createUseStyles({
+  editor: {
+    minHeight: 400,
+  },
+})
+
 export default defineComponent({
   setup() {
     const schemaRef: Ref<any> = ref(schema)
@@ -20,7 +28,9 @@ export default defineComponent({
       }
       schemaRef.value = schema
     }
+    const classesRef = useStyles()
     return () => {
+      const classes = classesRef.value
       const code = toJson(schemaRef.value)
       return (
         <div>
@@ -28,6 +38,7 @@ export default defineComponent({
             code={code}
             onChange={handleCodeChange}
             title="Schema"
+            class={classes.editor}
           ></MonacoEditor>
         </div>
       )
